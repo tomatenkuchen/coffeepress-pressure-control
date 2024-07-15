890d9b97d55ec9a986ea3fca3adde4493bedb2fe
tomatenkuchen <t.w92@gmx.de>
2024-07-15 06:00:49 +0200

    updated pressure sensor driver to a nostd friendly version.
    updated main to esp-hal 0.18.0 standart

--- software/src/main.rs
+++ software/src/main.rs
@@ -5,13 +5,14 @@ extern crate lps28dfw;
 
 use esp_backtrace as _;
 use esp_hal::{
-    analog::adc::{AdcConfig, Attenuation, ADC}, 
-    clock::ClockControl, 
-    delay::Delay, 
-    gpio::IO, 
+    analog::adc::{AdcConfig, Attenuation, Adc},
+    clock::ClockControl,
+    delay::Delay,
+    gpio::Io,
     i2c::I2C,
-    peripherals::Peripherals, 
-    prelude::*, 
+    peripherals::Peripherals,
+    prelude::*,
+    system::SystemControl,
 };
 use esp_println::println;
 
@@ -20,47 +21,46 @@ fn main() -> ! {
     // init peripherals access point
     let peripherals = Peripherals::take();
     // init system
-    let system = peripherals.SYSTEM.split();
+    let system = SystemControl::new(peripherals.SYSTEM);
     // init clock
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-    let i_heater_analog_pin = io.pins.gpio0.into_analog();
-    let v_grid_analog_pin = io.pins.gpio1.into_analog();
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
     let i2c_sda_pin = io.pins.gpio4;
     let i2c_scl_pin = io.pins.gpio5;
 
     // enable i2c for pressure sensor
-    let mut i2c = I2C::new(
+    let i2c = I2C::new(
         peripherals.I2C0,
         i2c_sda_pin,
         i2c_scl_pin,
         100u32.kHz(),
         &clocks,
+        None,
     );
 
     // set up pressure sensor
-    let mut p_sens = lps28dfw::LPS28DFW(i2c, lps28dfw::I2CAddress::Low, lps28dfw::OutputDataRate::Hz4, lps28dfw::Averaging::Over4);
+    let mut p_sens = lps28dfw::LPS28DFW::new(i2c, lps28dfw::I2CAddress::Low, lps28dfw::Range::Range4060hPa);
     // check if pressure sensor is alive
-    p_sens.identify();
+    p_sens.identify().unwrap();
 
     // get grid voltage input
     let mut adc1_config = AdcConfig::new();
-    let mut adc0_pin = adc1_config.enable_pin(i_heater_analog_pin, Attenuation::Attenuation11dB);
-    let mut adc1_pin = adc1_config.enable_pin(v_grid_analog_pin, Attenuation::Attenuation11dB);
-    let mut adc1 = ADC::new(peripherals.ADC1, adc1_config);
+    let mut adc_input_heater_current = adc1_config.enable_pin(io.pins.gpio0, Attenuation::Attenuation11dB);
+    let mut adc_input_grid_voltage = adc1_config.enable_pin(io.pins.gpio1, Attenuation::Attenuation11dB);
+    let mut adc1 = Adc::new(peripherals.ADC1, adc1_config);
 
-    let mut delay = Delay::new(&clocks);
+    let delay = Delay::new(&clocks);
 
     loop {
         // read adc
-        let i_grid_value: u16 = nb::block!(adc1.read(&mut adc0_pin)).unwrap();
-        let v_grid_value: u16 = nb::block!(adc1.read(&mut adc1_pin)).unwrap();
+        let i_grid_value: u16 = nb::block!(adc1.read_oneshot(&mut adc_input_heater_current)).unwrap();
+        let v_grid_value: u16 = nb::block!(adc1.read_oneshot(&mut adc_input_grid_voltage)).unwrap();
 
         // read pressure sensor
         let p = p_sens.read_pressure().unwrap();
         println!("ADC1: v_grid = {}, i_grid = {}",v_grid_value, i_grid_value);
         println!("I2C0 : data array = {:?}", p);
-        delay.delay_ms(2000u32);
+        delay.delay_millis(2000u32);
     }
 }
